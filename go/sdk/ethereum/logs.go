129e6a103ed9a806de5860c98b7f19b4f4a592d4
Tal Kol <talkol@gmail.com>
2018-12-09 16:07:47 +0200

    Improved naming

--- go/sdk/ethereum/logs.go
+++ go/sdk/ethereum/logs.go
@@ -3,7 +3,7 @@ package ethereum
 import "github.com/orbs-network/orbs-contract-sdk/go/context"
 
 // returns a single log (single emit), in future we will add a version that returns multiple
-func GetLog(contractAddress string, jsonAbi string, ethTransactionId string, eventName string, out interface{}) {
+func GetTransactionLog(contractAddress string, jsonAbi string, ethTransactionId string, eventName string, out interface{}) {
 	contextId, handler, permissionScope := context.GetContext()
-	handler.SdkEthereumGetLog(contextId, permissionScope, contractAddress, jsonAbi, ethTransactionId, eventName, out)
+	handler.SdkEthereumGetTransactionLog(contextId, permissionScope, contractAddress, jsonAbi, ethTransactionId, eventName, out)
 }
