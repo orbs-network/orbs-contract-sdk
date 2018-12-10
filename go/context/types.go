f24a8d989c0c27884fb3a66c730e4db6fd3f1448
Tal Kol <talkol@gmail.com>
2018-12-11 00:40:45 +0200

    Added events methods support

--- go/context/types.go
+++ go/context/types.go
@@ -5,12 +5,13 @@ type ContextId uint32 // TODO: change to []byte (in protos too)
 type PermissionScope uint16
 
 const (
-	PERMISSION_SCOPE_SYSTEM PermissionScope = 1
+	PERMISSION_SCOPE_SYSTEM  PermissionScope = 1
 	PERMISSION_SCOPE_SERVICE PermissionScope = 2
 )
 
 type ContractInfo struct {
-	PublicMethods    []interface{}
-	SystemMethods    []interface{}
-	Permission PermissionScope
+	PublicMethods []interface{}
+	SystemMethods []interface{}
+	EventsMethods []interface{}
+	Permission    PermissionScope
 }
