450fd383b592d7bf8fc7e14352ce80b231587e67
Kirill Maksimov <kirill@orbs.com>
2019-05-05 19:16:40 +0300

    Remove gamma-cli

--- docker-test.sh
+++ docker-test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -x
 
-docker rm -f $(docker ps -aq)
-export LOCAL_IP=$(python -c "import socket; print socket.gethostbyname(socket.gethostname())")
-
 docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from test
