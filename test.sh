8eccfbcb823ac543e00dddf73d15df0e9b5c74d3
itamar Arjuan <itamar.arjuan@gmail.com>
2019-07-25 15:44:15 +0300

    ci mods

--- test.sh
+++ test.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -e
 
-# This test requires a global setup in the shape of launching a Docker container before we run go test.
-
-# Shut down any existing Gamma server container
-echo "Clearing up any existing gamma servers.."
-docker ps -a | grep orbsnetwork\/gamma | awk '{print $1}' | xargs docker rm -fv
-
-echo "Starting up gamma server.."
-docker run -d -p "8080:8080" orbsnetwork/gamma:experimental
-
-echo "Gamma server started and listening on port 8080"
-
+echo "Sleeping 5 seconds to allow Gamma server to wake up..."
 sleep 5
 
 echo "Running SDK tests..."
